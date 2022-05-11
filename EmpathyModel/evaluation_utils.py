09218ae0b2ac1f89726cdd2aecdb1f6c727039d7
Baasit Sharief <baasit8@gmail.com>
2022-05-10 23:51:02 -0400

    first submission

--- EmpathyModel/evaluation_utils.py
+++ EmpathyModel/evaluation_utils.py
@@ -1,3 +1,12 @@
+'''I/we certify that the code and data in this assignment were generated
+independently, using only the tools and resources defined in the course
+and that I/we did not receive any external help, coaching or contributions
+during the production of this work."
+
+References: 
+1. https://github.com/lvwerra/trl/blob/master/nbs/04-gpt2-sentiment-ppo-training.ipynb
+2. https://github.com/behavioral-data/Empathy-Mental-Health'''
+
 from sklearn.metrics import f1_score
 import numpy as np
 import pandas as pd
